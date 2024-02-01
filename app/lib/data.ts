f3ace2632cea18bd4d0cbe20a6ba3b637c2f7f92
Berkay Anduv <sygrok@hotmail.com>
2024-02-01 13:17:32 +0300

    test

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { log } from 'console';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
